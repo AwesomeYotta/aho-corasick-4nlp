b3a08aeae7a6969b53e189f9badf14010b2031ad
changnet <940477049@qq.com>
2021-04-28 14:44:07 +0800

    fix const T_FLAGS error in windows

--- _acism.h
+++ _acism.h
@@ -72,7 +72,7 @@ static const TRAN IS_MATCH  = (TRAN)1 << (8*sizeof(TRAN) - 1);
 static const TRAN IS_SUFFIX = (TRAN)1 << (8*sizeof(TRAN) - 2);
 // windows C not allow this
 // static const TRAN T_FLAGS   = IS_MATCH | IS_SUFFIX;
-static const TRAN T_FLAGS   = ((TRAN)1 << (8*sizeof(TRAN) - 1)) | ((TRAN)1 << (8*sizeof(TRAN) - 2))
+static const TRAN T_FLAGS   = ((TRAN)1 << (8*sizeof(TRAN) - 1)) | ((TRAN)1 << (8*sizeof(TRAN) - 2));
 
 
 typedef struct { STATE state; STRNO strno; } STRASH;
